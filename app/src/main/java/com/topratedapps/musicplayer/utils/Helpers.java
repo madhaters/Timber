b45887705665ca3587e948e95b99922501da1b9b
Osama aka bullhead <m.osamabinomar@gmail.com>
2017-11-13 16:07:26 +0500

    changed app name

--- app/src/main/java/com/topratedapps/musicplayer/utils/Helpers.java
+++ app/src/main/java/com/topratedapps/musicplayer/utils/Helpers.java
@@ -17,9 +17,6 @@ package com.topratedapps.musicplayer.utils;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -57,10 +54,6 @@ public class Helpers {
 
     public static class AboutDialog extends DialogFragment {
 
-        String urlgooglelus = "https://plus.google.com/u/0/+NamanDwivedi14";
-        String urlcommunity = "https://plus.google.com/communities/111029425713454201429";
-        String urltwitter = "https://twitter.com/naman1405";
-        String urlgithub = "https://github.com/naman14";
         String urlsource = "https://github.com/naman14/Timber/issues";
 
         public AboutDialog() {
@@ -75,11 +68,8 @@ public class Helpers {
 
             TextView appversion = (TextView) aboutBodyView.findViewById(R.id.app_version_name);
 
-            TextView googleplus = (TextView) aboutBodyView.findViewById(R.id.googleplus);
-            TextView twitter = (TextView) aboutBodyView.findViewById(R.id.twitter);
-            TextView github = (TextView) aboutBodyView.findViewById(R.id.github);
+
             TextView source = (TextView) aboutBodyView.findViewById(R.id.source);
-            TextView community = (TextView) aboutBodyView.findViewById(R.id.feature_request);
 
             TextView dismiss = (TextView) aboutBodyView.findViewById(R.id.dismiss_dialog);
             dismiss.setOnClickListener(new View.OnClickListener() {
@@ -88,37 +78,9 @@ public class Helpers {
                     dismiss();
                 }
             });
-            googleplus.setPaintFlags(googleplus.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
-            twitter.setPaintFlags(twitter.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
-            github.setPaintFlags(github.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
 
-            googleplus.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent i = new Intent(Intent.ACTION_VIEW);
-                    i.setData(Uri.parse(urlgooglelus));
-                    startActivity(i);
-                }
 
-            });
-            twitter.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent i = new Intent(Intent.ACTION_VIEW);
-                    i.setData(Uri.parse(urltwitter));
-                    startActivity(i);
-                }
 
-            });
-            github.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent i = new Intent(Intent.ACTION_VIEW);
-                    i.setData(Uri.parse(urlgithub));
-                    startActivity(i);
-                }
-
-            });
             source.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -127,22 +89,7 @@ public class Helpers {
                     startActivity(i);
                 }
             });
-            community.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent i = new Intent(Intent.ACTION_VIEW);
-                    i.setData(Uri.parse(urlcommunity));
-                    startActivity(i);
-                }
-            });
-            try {
-                PackageInfo pInfo = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0);
-                String version = pInfo.versionName;
-                int versionCode = pInfo.versionCode;
-                appversion.setText("Timber " + version);
-            } catch (PackageManager.NameNotFoundException e) {
-                e.printStackTrace();
-            }
+
 
             return new AlertDialog.Builder(getActivity())
                     .setView(aboutBodyView)
