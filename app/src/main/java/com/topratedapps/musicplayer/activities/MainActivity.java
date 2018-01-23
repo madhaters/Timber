b9409b4d5fd656d2533e865e11a553498e3625cb
Osama Bin Omar (bullhead) <m.osamabinomar@gmail.com>
2018-01-23 12:35:12 +0500

    fixed permission for reordering list

--- app/src/main/java/com/topratedapps/musicplayer/activities/MainActivity.java
+++ app/src/main/java/com/topratedapps/musicplayer/activities/MainActivity.java
@@ -228,13 +228,13 @@ public class MainActivity extends BaseActivity implements ATEActivityThemeCustom
         if (Nammu.checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE)) {
             loadEverything();
         } else {
-            if (Nammu.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            if (Nammu.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
                 Snackbar.make(panelLayout, "Timber will need to read external storage to display songs on your device.",
                         Snackbar.LENGTH_INDEFINITE)
                         .setAction("OK", new View.OnClickListener() {
                             @Override
                             public void onClick(View view) {
-                                Nammu.askForPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE, permissionReadstorageCallback);
+                                Nammu.askForPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE, permissionReadstorageCallback);
                             }
                         }).show();
             } else {
